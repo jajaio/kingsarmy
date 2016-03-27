950baae2679372073423d0e6b427146a645479f4
jajaio <jackm2580@gmail.com>
2016-03-26 22:17:50 -0400

    save

---
+++ classes.py
@@ -0,0 +1,31 @@
+import colors as c
+author="jajaio"
+
+class Thing():
+	hp=None #Health
+	agi=None #Agility
+	deff=None #Defense
+	att=None #Attack
+	mp=None #Magic Power (Ammount of times you can heal)
+
+class Player(Thing):
+    xp=0
+    name=None
+
+class Foe(Thing):
+    mname="Test mob name"
+
+def show_player():    
+    yext='''
+    {p.name} Stats:
+    Health: {p.hp}
+    Agility: {p.agi}
+    Attack: {p.agi}
+    Defense: {p.deff}
+    Magic: {p.mp}
+    Gold: {p.gold}
+    '''
+    print(yext.format(p=Player()))
+
+if __name__=='__main__':
+    show_player()
---
+++ save.py
@@ -0,0 +1,18 @@
+import json
+import classes as cl
+author="jajaio"
+
+def save_game():
+	with open('player.json', 'w') as pfile:
+		pfile.write(json.dumps({
+            "hp":cl.Player.hp,
+            "agi":cl.Player.agi,
+            "deff":cl.Player.deff,
+            "att":cl.Player.att,
+            "mp":cl.Player.mp,
+            "gold":cl.Player.gold,
+            "name":cl.Player.name
+            }))
+        
+if __name__=='__main__':
+    save_game()
