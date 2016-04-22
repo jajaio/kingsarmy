7714b62f286825f4b4c176bab0f59f47e809d2e8
jajaio <jackm2580@gmail.com>
2016-04-22 12:37:34 -0400

    save

--- dotdotdot.py
+++ dotdotdot.py
@@ -16,5 +16,19 @@ def normal():
     t.sleep(.5)
     print(c.clear)
 
+def slow():
+    print(c.reset)
+    print(c.clear)
+    print('.')
+    t.sleep(2)
+    print('..')
+    t.sleep(2)
+    print(c.clear)
+    print('...')
+    t.sleep(2)
+    print(c.clear)
 if __name__=='__main__':
+    print('Normal Speed')
     normal()
+    print('Slow Speed')
+    slow()
